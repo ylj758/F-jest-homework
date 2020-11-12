437179641122e5606634ca9a1aa1ed4dd52dd1d1
ylj758 <758766951@qq.com>
2020-11-12 23:05:27 +0800

    [ylj]:implement TODO13

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,12 @@
+import axios from "axios";
+import getUsers from "../users";
+
+jest.mock("axios");
 describe("users", () => {
-  test("should get users data with mock axios get", () => {
-    // TODO 13: add async test with manual mock
-  });
+    test("should get users data with mock axios get", () => {
+        // TODO 13: add async test with manual mock
+        const user = {name: "marry", password: "123"};
+        axios.get.mockResolvedValue({data: user});
+        expect(getUsers()).resolves.toEqual(user);
+    });
 });
